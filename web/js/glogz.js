8350a22443c2b885c4fac74d163d0495f5152e90
plouc <benitteraphael@gmail.com>
2013-02-04 01:49:51 +0100

    Selectable stream + logs visibility toggle

--- web/js/glogz.js
+++ web/js/glogz.js
@@ -10,6 +10,12 @@
 
   // keyboard keys setup
   var keyCodeChars = {
+    27: 'esc',
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
     65: 'a',
     66: 'b',
     67: 'c',
@@ -18,6 +24,9 @@
     70: 'f',
     72: 'h',
     73: 'i',
+    76: 'l',
+    83: 's',
+    84: 't',
     88: 'x',
     89: 'y',
     90: 'z'
@@ -34,9 +43,11 @@
    * @constructor
    */
   var Glogz = function(logs, streams, socket) {
-    this.logs    = logs;
-    this.streams = streams;
-    this.socket  = socket;
+    this.logs         = logs;
+    this.streams      = streams;
+    this.socket       = socket;
+    this.context      = null;
+    this.contextScope = null;
 
     this.initElements()
         .initHandlers()
@@ -58,8 +69,7 @@
       'logs':            $('#logs'),
       'logList':         $('.log-list'),
       'streamList':      $('.stream-list'),
-      'craftLogList':    $('.craft-log-list'),
-      'craftButton':     $('.craft-button'),
+      'clearButton':     $('.clear-button'),
       'craftPane':       $('#craft'),
       'helpButton':      $('.help-button'),
       'helpPane':        $('#help'),
@@ -83,16 +93,16 @@
 
       var $activeElement = $(event.currentTarget.activeElement);
 
-      //console.log($activeElement, event.keyCode);
+      var keySymbol = keyCodeToChar(event.keyCode);
 
       if (!$activeElement.is('input')) {
-        switch (keyCodeToChar(event.keyCode)) {
+        switch (keySymbol) {
           case 'a':
             self.openPane('about');
             break;
 
           case 'c':
-            self.openPane('craft');
+            self.clear();
             break;
 
           case 'f':
@@ -107,13 +117,51 @@
             self.toggleInfos();
             break;
 
+          case 'l':
+            self.showInfos()
+                .enterContext(self.elements.logList);
+            break;
+
+          case 's':
+            self.showInfos()
+                .enterContext(self.elements.streamList);
+            break;
+
+          case 'space':
+            if (self.contextScope !== null && self.contextScope.is('.stream')) {
+              var streamName = self.contextScope.attr('id').substr(7);
+              self.setStream(streamName);
+            }
+            break;
+
+          case 't':
+            if (self.contextScope !== null && self.contextScope.is('.log')) {
+              var logName = self.contextScope.attr('id').substr(4);
+              if (!self.contextScope.hasClass('hidden')) {
+                self.contextScope.addClass('hidden');
+                self.hideLogs(logName);
+              } else {
+                self.contextScope.removeClass('hidden');
+                self.showLogs(logName);
+              }
+            }
+            break;
+
           case 'x':
             self.closePanes();
+            break;
+
+          case 'up':
+            self.contextPrev();
+            break;
+
+          case 'down':
+            self.contextNext();
             break;
         }
       } else {
         // esc
-        if (event.keyCode == 27) {
+        if (keySymbol == 'esc') {
           self.elements.q.blur();
         }
       }
@@ -128,7 +176,11 @@
       self.showInfos();
     });
 
-    _.each(['craft', 'help', 'about'], function(paneName) {
+    this.elements.clearButton.on('click', function() {
+      self.clear();
+    });
+
+    _.each(['help', 'about'], function(paneName) {
       self.elements[paneName + 'Button'].on('click', function() {
         self.openPane(paneName);
       });
@@ -144,18 +196,24 @@
 
     var i = 0, self = this;
 
-    _.each(this.logs, function(log, logId) {
+    _.each(this.logs, function(log) {
       log.color = colorRange(i);
-      self.elements.logList.append('<li>' +
-        '<span class="chip" style="background-color: ' + log.color + '"></span>' +
-        log.name +
+      self.elements.logList.append('<li class="log" id="log-' + log.name + '">' +
+        '<span class="chip-cell">' +
+          '<span class="chip" style="background-color: ' + log.color + '"></span>' +
+        '</span>' +
+        '<span class="log-visibility"></span>' +
+        '<span class="log-name">' + log.name + '</span>' +
       '</li>');
-      self.elements.craftLogList.append('<li>' + log.name + '</li>');
       i++;
     });
 
     _.each(this.streams, function(stream) {
-      self.elements.streamList.append('<li>' + stream.name + '</li>');
+      var $stream = $('<li class="stream" id="stream-' + stream.name + '">' + stream.name + '</li>');
+      $stream.on('click', function() {
+        self.setStream(stream.name);
+      });
+      self.elements.streamList.append($stream);
     });
 
     return this;
@@ -203,15 +261,135 @@
   /**
    * @return Glogz
    */
+  Glogz.prototype.clear = function() {
+    this.elements.logs.find('.log').remove();
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.enterContext = function(context) {
+    if (this.contextScope !== null) {
+      this.contextScope.removeClass('has-scope');
+    }
+
+    this.context      = context;
+    this.contextScope = this.context.find('> *:first-child');
+
+    this.contextScope.addClass('has-scope')
+                     .focus();
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.hideLogs = function(logName) {
+    $('.log-line-' + logName).css('display', 'none');
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.showLogs = function(logName) {
+    if (logName) {
+      $('.log-line-' + logName).css('display', 'block');
+    } else {
+      $('.log-line').css('display', 'block');
+    }
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.contextPrev = function() {
+    if (this.contextScope !== null) {
+      this.contextScope.removeClass('has-scope');
+
+      var prev = this.contextScope.prev();
+      if (prev.length) {
+        this.contextScope = prev;
+      } else {
+        this.contextScope = this.contextScope.parent().find('> *:last-child');
+      }
+
+      this.contextScope.addClass('has-scope')
+                       .focus();
+    }
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.contextNext = function() {
+    if (this.contextScope !== null) {
+      this.contextScope.removeClass('has-scope');
+
+      var next = this.contextScope.next();
+      if (next.length) {
+        this.contextScope = next;
+      } else {
+        this.contextScope = this.contextScope.parent().find('> *:first-child');
+      }
+
+      this.contextScope.addClass('has-scope')
+                       .focus();
+    }
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
+  Glogz.prototype.leaveContext = function() {
+    this.contextScope.removeClass('has-scope');
+
+    this.context      = null;
+    this.contextScope = null;
+
+    return this;
+  };
+
+  /**
+   * @return Glogz
+   */
   Glogz.prototype.setStream = function(streamName) {
     var streamLogs = this.streams[streamName].logs,
         self       = this;
 
+    this.elements.streamList.find('li').removeClass('active');
+    this.elements.streamList.find('#stream-' + streamName).addClass('active');
+
+    this.elements.logList.find('li').each(function() {
+      $(this).addClass('disabled')
+             .find('.log-visibility')
+             .removeClass('visible')
+             .removeClass('hidden')
+             .addClass('locked')
+    });
+
+    _.each(this.logs, function(logName) {
+      self.socket.removeListener('log.' + logName);
+    });
+
     _.each(streamLogs, function(logName) {
+
+      $('#log-' + logName).removeClass('disabled');
+
       self.socket.on('log.' + logName, function(data) {
         data = data.split("\n");
         _.each(data, function(line) {
-          self.elements.logs.append('<span class="log">' +
+          self.elements.logs.append('<span class="log-line log-line-' + logName + '">' +
             '<span class="chip" style="background-color: ' + self.logs[logName].color + ';"></span>' +
             line +
           '</span>');
